62036e978035e8059e6b7c715d8b1f7e0d222cc2
Kirby Shabaga <kirby.shabaga@me.com>
2020-12-26 19:14:09 -0800

    feat: Define Classes and populate dinosaurs array from dino.json
    Defined Animal, Human, and Dinosaur classes
    
    Added methods to fetch dino json and instantiate an array of Dinosaurs.

--- app.js
+++ app.js
@@ -1,9 +1,59 @@
+// https://github.com/udacity/Javascript
+// https://medium.com/javascript-scene/javascript-factory-functions-vs-constructor-functions-vs-classes-2f22ceddf33e
+// https://developer.mozilla.org/en-US/docs/Web/API/Fetch_API/Using_Fetch
 
-    // Create Dino Constructor
+// ---------------------------------------------------------------------------
 
+class Animal {
+    constructor(species, height, weight, diet) {
+        this.species = this.species;
+        this.height = height;
+        this.weight = weight;
+        this.diet = diet;
+    }
+}
 
-    // Create Dino Objects
+class Human extends Animal {
+    constructor(species, height, weight, diet, name) {
+        super(species, height, weight, diet);
+        this.name = name;
+    }
+}
+class Dino extends Animal {
+    constructor(species, weight, height, diet, where, when, fact) {
+        super(species, height, weight, diet);
 
+        this.where = where;
+        this.when = when;
+        this.fact = fact;
+    }
+}
+
+const dinosaurs = [];
+
+// ---------------------------------------------------------------------------
+
+// Read JSON data and then parse into Dinosaur instances
+async function fetchDinosJSON(url) {
+    const response = await fetch(url);
+
+    if (!response.ok) {
+        throw new Error(`An error has occured: ${response.status}`);
+    }
+
+    const data = await response.json();
+    return data;
+}
+
+fetchDinosJSON('./dino.json').then(data => {
+    data.Dinos.map(function parseDino(dino) {
+        dinosaurs.push(new Dino(dino.species, dino.weight, dino.height, dino.diet, dino.where, dino.when, dino.fact));
+    });
+}).catch(error => {
+    alert('Fetch Error\n' + error.message);
+});
+
+// ---------------------------------------------------------------------------
 
     // Create Human Object
 
@@ -13,17 +63,17 @@
     // Create Dino Compare Method 1
     // NOTE: Weight in JSON file is in lbs, height in inches. 
 
-    
+
     // Create Dino Compare Method 2
     // NOTE: Weight in JSON file is in lbs, height in inches.
 
-    
+
     // Create Dino Compare Method 3
     // NOTE: Weight in JSON file is in lbs, height in inches.
 
 
     // Generate Tiles for each Dino in Array
-  
+
         // Add tiles to DOM
 
     // Remove form from screen
